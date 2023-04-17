4a2c027a9477e998b1618af2729558a122faf341
lhthien09 <56301506+lhthien09@users.noreply.github.com>
2023-04-17 20:15:41 +0200

    Add files via upload

---
+++ Python_DevOPS/Higher_Lower game/main.py
@@ -0,0 +1,50 @@
+import art
+from game_data import data
+import random 
+from replit import clear
+
+is_over = 0 
+current_score = 0
+count_A = 0
+count_B = 0
+
+
+print(art.logo)
+x1 = random.choice(data)
+
+def game_play(x1):
+     global data, current_score
+     data.remove(x1)
+     x2 = random.choice(data)
+     count_A = x1["follower_count"]
+     count_B = x2["follower_count"]
+     if current_score > 0:
+        print('''
+        ***************************************
+        ***************************************
+        ***************************************
+        ''')
+        print(f"\nYou're right. Current score {current_score}.")
+     print(f'Compare A: {x1["name"]}, {x1["description"]}, {x1["country"]}.')
+     print(art.vs)
+     print(f'Compare B: {x2["name"]}, {x2["description"]}, {x2["country"]}.')
+   
+     choose = input("Who has more followers? Type 'A' or 'B': ")
+     if count_A > count_B:
+       answer = "A"
+     else: 
+       answer = "B"
+     if choose == answer:
+       current_score +=1
+       game_play(x2)
+     else: 
+       clear()
+       print(art.logo)
+       print(f"Sorry, that's wrong. Final score: {current_score}")
+       return 
+      
+
+is_over = game_play(x1)
+       
+
+      
